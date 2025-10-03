171da059d8507e4f8f5cd402a2502611fb193265
Marzan Ahmed <141250984+MarzzzSiam@users.noreply.github.com>
2025-10-03 19:07:52 +0600

    Add files via upload

---
+++ do_you_love_me.js
@@ -0,0 +1,34 @@
+const questionContainer = document.querySelector(".question-container");
+const resultContainer = document.querySelector(".result-container");
+const gifResult = document.querySelector(".gif-result");
+const heartLoader = document.querySelector(".cssload-main");
+const yesBtn = document.querySelector(".js-yes-btn");
+const noBtn = document.querySelector(".js-no-btn");
+const startGifContainer = document.querySelector(".start-gif-container");
+
+// Move No button randomly
+noBtn.addEventListener("mouseover", () => {
+  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
+  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+
+  noBtn.style.left = `${newX}px`;
+  noBtn.style.top = `${newY}px`;
+});
+
+// Yes button functionality
+yesBtn.addEventListener("click", () => {
+  questionContainer.style.display = "none";
+  heartLoader.style.display = "inherit";
+
+  setTimeout(() => {
+    heartLoader.style.display = "none";
+    resultContainer.style.display = "block";
+    gifResult.play();
+  }, 3000);
+});
+
+// Show question after 2 seconds, hide start GIF
+setTimeout(() => {
+  startGifContainer.style.display = "none";
+  questionContainer.style.display = "block";
+}, 2000);
